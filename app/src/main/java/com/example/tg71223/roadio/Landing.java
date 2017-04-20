fc3a7df26d9df8591dbe80cec0ca7ef11d84f1a0
Tanner Garrett <tanner@garrett.org>
2017-04-19 22:08:25 -0700

    Initial commit

---
+++ app/src/main/java/com/example/tg71223/roadio/Landing.java
@@ -0,0 +1,31 @@
+package com.example.tg71223.roadio;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Landing extends Fragment {
+
+
+    public Landing() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View thisView = inflater.inflate(R.layout.fragment_landing2, container, false);
+        return thisView;
+    }
+
+}
